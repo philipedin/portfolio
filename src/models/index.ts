5d59b4d91527040a503028b5f9b4cc32e92c8ae1
Philip <philip.edin@gmail.com>
2018-07-07 19:12:40 +0200

    Add skill-model

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,9 +1,11 @@
 import IEdge from './edge';
 import IWork from './work';
 import IImage from './image';
+import ISkill from './skill';
 
 export {
   IEdge,
   IWork,
   IImage,
+  ISkill,
 };
---
+++ src/models/skill.ts
@@ -0,0 +1,6 @@
+import { IImage } from '.';
+
+export default interface ISkill {
+  name: string;
+  image: IImage;
+}
