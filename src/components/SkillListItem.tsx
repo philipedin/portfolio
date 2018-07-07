e1ba3c3624ca28a31bb89562cbad654584d2ec2c
Philip <philip.edin@gmail.com>
2018-07-07 22:32:17 +0200

    Improve appearance of skill list items

--- src/components/SkillListItem.tsx
+++ src/components/SkillListItem.tsx
@@ -13,6 +13,7 @@ const Wrapper = styled.div`
   height: 90px;
   width: 300px;
   margin: 15px;
+  padding: 10px;
 
   transition: all 0.3s cubic-bezier(.25,.8,.25,1);
   box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
@@ -23,13 +24,13 @@ const Wrapper = styled.div`
 `;
 
 const Left = styled.div`
-  padding: 10px;
+  display: inline-block;
 `;
 
 const Right = styled.div`
-  padding: 10px;
-
+  flex: 1;
   display: flex;
+  justify-content: center;
   align-items: center;
 `;
 
