cd3fcef2a58bff938cc4a1133255db9c8a5994ce
Philip <philip.edin@gmail.com>
2018-07-10 16:00:57 +0200

    Transition applies to all on WorkListItem

--- src/components/WorkListItem.tsx
+++ src/components/WorkListItem.tsx
@@ -29,7 +29,7 @@ const ImageContainer = styled.div`
 
   background: #fff;
 
-  transition: box-shadow 0.3s cubic-bezier(.25,.8,.25,1);
+  transition: all 0.3s cubic-bezier(.25,.8,.25,1);
 
   box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
 
